6e98738fd526ec2135650afa9cb9ccfdcbcf73e3
hunterwr <rog19011@byui.edu>
2022-08-11 15:18:40 -0600

    extra '

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -23,6 +23,6 @@ st.text_input("What is the content name", key="ad_id")
 st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}')
 
 if st.button('Copy URL'):
-    text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}''
+    text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
     pyperclip.copy(text_to_be_copied)
     st.write('Link copied!')
