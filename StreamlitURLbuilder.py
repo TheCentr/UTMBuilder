2b7d3c20f895ca4d634ea4d59d591bcb6625ed14
hunterwr <rog19011@byui.edu>
2022-08-11 14:52:02 -0600

    display URL

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -3,7 +3,7 @@
 import pandas as pd
 import streamlit as st
 
-st.write("The Centr - Add UTMs to your URL")
+st.markdown("# The Centr - Add UTMs to your URL 🎈")
 
 st.text_input("Enter the original (naked) URL", key="ogurl")
 
@@ -16,6 +16,9 @@ st.session_state.medium = option2
 option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
 st.session_state.campaign = option3
 
-st.text_input("What is the ad name", key="ad_id")
+st.text_input("What is the content name", key="ad_id")
+
+
+st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.brand}+&utm_medium={st.session_state.medium}+&utm_campaign={st.session_state.campaign}+&utm_content={st.session_state.ad_id}')
 
 
