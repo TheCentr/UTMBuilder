20aa0fceac75c6f2ad309eba65a8ba9bdb987c78
hunterwr <rog19011@byui.edu>
2022-08-11 15:39:51 -0600

    yikes

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import streamlit as st
+import xsel
 import pyperclip
 
 st.markdown("# The Centr - Add UTMs to your URL 🎈")
@@ -22,7 +23,7 @@ st.text_input("What is the content name", key="ad_id")
 
 st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}')
 
-if st.button('Copy URL'):
-    text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
-    pyperclip.copy('test')
-    st.write('Link copied!')
+# if st.button('Copy URL'):
+#     text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
+#     pyperclip.copy('test')
+#     st.write('Link copied!')
