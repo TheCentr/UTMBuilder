3ca3943eba845f30b3998227bd993a6585d7b664
hunterwr <rog19011@byui.edu>
2022-08-11 14:43:29 -0600

    updates to options

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -5,15 +5,15 @@ import streamlit as st
 
 st.write("The Centr - Add UTMs to your URL")
 
+st.text_input("Enter the original (naked) URL", key="ogurl")
 
-
-option = st.selectbox('Please select a brand', ('facebook', 'youtube', 'linkedin'))
+option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram'))
 st.session_state.brand = option
 
-option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner'))
+option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner', 'email'))
 st.session_state.medium = option2
 
-option3 = st.selectbox('Please select a campaign', ('teamsourcing'))
+option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
 st.session_state.campaign = option3
 
 st.text_input("What is the ad name", key="ad_id")
