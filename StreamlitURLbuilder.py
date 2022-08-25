f41c5228f5aadc143ae4f02c1cb74ddc341a4cc0
hunterwr <rog19011@byui.edu>
2022-08-25 09:12:47 -0600

    try agian

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -10,21 +10,21 @@ if st.checkbox('I am here to create my personal Omni-Support URL'):
     #st.text_input("Enter the original (naked) URL", key="ogurl", placeholder='For example: https://thecentr.com/teamsourcing', value='https://thecentr.com/teamsourcing')
     st.session_state.ogurl = 'https://thecentr.com/teamsourcing'
 
-    #option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram', 'direct'))
+    #option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram', 'directsocialmessage'))
     st.session_state.source = 'direct'
 
     #option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner', 'email', 'socialmessage'))
-    st.session_state.medium = 'socialmessage'
+    #st.session_state.medium = 'socialmessage'
 
     #option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
     st.session_state.campaign = 'teamsourcing'
 
-    st.text_input("What is your username", key="user", placeholder='For example: cwatson')
+    st.text_input("What is your username", key="medium", placeholder='For example: cwatson')
 
     st.markdown("#### After fill out the required information, copy this link")
 
     if st.button('Display URL'):
-        st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.user}')
+        st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content=none')
 
     # if st.button('Copy URL'):
     #     text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
