a2ea45b1fbf9162a165c08361fccf39d92c1bc86
hunterwr <rog19011@byui.edu>
2022-08-25 09:03:36 -0600

    added omni support section

--- StreamlitURLbuilder.py
+++ StreamlitURLbuilder.py
@@ -6,25 +6,52 @@ import pyperclip
 
 st.markdown("# The Centr - Add UTMs to your URL 🎈")
 
-st.text_input("Enter the original (naked) URL", key="ogurl", placeholder='For example: https://thecentr.com/teamsourcing', value='https://thecentr.com/teamsourcing')
+if st.checkbox('I am here to create my personal Omni-Support URL'):
+    #st.text_input("Enter the original (naked) URL", key="ogurl", placeholder='For example: https://thecentr.com/teamsourcing', value='https://thecentr.com/teamsourcing')
 
-option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram'))
-st.session_state.source = option
+    #option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram', 'direct'))
+    st.session_state.source = 'direct'
 
-option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner', 'email'))
-st.session_state.medium = option2
+    option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner', 'email', 'socialmessage'))
+    st.session_state.medium = 'socialmessage'
 
-option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
-st.session_state.campaign = option3
+    option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
+    st.session_state.campaign = 'teamsourcing'
 
-st.text_input("What is the content name", key="ad_id")
+    st.text_input("What is your username", key="user", placeholder='For example: cwatson')
 
-st.markdown("#### After fill out the required information, copy this link")
+    st.markdown("#### After fill out the required information, copy this link")
 
-if st.button('Display URL'):
-    st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}')
+    if st.button('Display URL'):
+        st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}')
 
-# if st.button('Copy URL'):
-#     text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
-#     pyperclip.copy('test')
-#     st.write('Link copied!')
+    # if st.button('Copy URL'):
+    #     text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
+    #     pyperclip.copy('test')
+    #     st.write('Link copied!')
+
+
+else:
+    st.warning('Click the checkbox if you are here from omni-support')
+    st.text_input("Enter the original (naked) URL", key="ogurl", placeholder='For example: https://thecentr.com/teamsourcing', value='https://thecentr.com/teamsourcing')
+
+    option = st.selectbox('Please select a source', ('facebook', 'youtube', 'linkedin', 'instagram', 'direct'))
+    st.session_state.source = option
+
+    option2 = st.selectbox('Please select a medium', ('cpc', 'organic', 'banner', 'email', 'socialmessage'))
+    st.session_state.medium = option2
+
+    option3 = st.selectbox('Please select a campaign', ('teamsourcing', 'other'))
+    st.session_state.campaign = option3
+
+    st.text_input("What is the content name", key="ad_id")
+
+    st.markdown("#### After fill out the required information, copy this link")
+
+    if st.button('Display URL'):
+        st.write(f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}')
+
+    # if st.button('Copy URL'):
+    #     text_to_be_copied = f'{st.session_state.ogurl}?utm_source={st.session_state.source}&utm_medium={st.session_state.medium}&utm_campaign={st.session_state.campaign}&utm_content={st.session_state.ad_id}'
+    #     pyperclip.copy('test')
+    #     st.write('Link copied!')
